f3fb390554f971a0323e97f2265e010148ef7640
shuantt <tehsuan.tht@gmail.com>
2026-09-20 01:00:41 +0800

    refactor(about): 統一管理介紹與聯絡內容
    - 將聯絡段落移至作者 MDX，與 About 介紹一起編輯
    - 沿用右側內文欄位，統一段落寬度與對齊
    - 移除頁面中重複的 JSX 與未使用的 Link import

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,6 @@ import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import siteMetadata from '@/data/siteMetadata'
 import { genPageMetadata } from 'app/seo'
-import Link from '@/components/Link'
 
 const description =
   'Shuan Tseng，設計背景出身、近四年經驗的全端工程師與創作者。這裡記錄開發、設計與生活，歡迎交流專案與工作機會。'
@@ -46,26 +45,6 @@ export default function Page() {
       <AuthorLayout content={mainContent}>
         <MDXLayoutRenderer code={author.body.code} />
       </AuthorLayout>
-      <div className=" py-4">
-        <h2 className="mb-4 text-xl font-semibold">有想與我交流的事情嗎？</h2>
-        <p className="leading-7 text-gray-600 dark:text-gray-400">
-          不論是專案合作、工作機會，或者是對程式開發、設計、生活或只是想與我隨性聊聊，都歡迎透過{' '}
-          <Link
-            href="mailto:tehsuan.tht@gmail.com"
-            className="inline-block py-2 font-medium text-primary-600 underline underline-offset-4 dark:text-primary-400"
-          >
-            Email
-          </Link>{' '}
-          或到{' '}
-          <Link
-            href="/guestbook"
-            className="inline-block py-2 font-medium text-primary-600 underline underline-offset-4 dark:text-primary-400"
-          >
-            Guestbook
-          </Link>{' '}
-          留話給我，我會盡快回覆你！記得留下你的個人網站或社群連結，讓我有機會回訪喔 :)
-        </p>
-      </div>
     </>
   )
 }
